906d3658b93fbe8391fc144fa360613d1e993c6b
DarioCostaAlves <dario.c.alves22@gmail.com>
2024-06-07 22:01:38 +0100

    improved home screen design. added navController

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonLabel, IonIcon, IonItem, IonList, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private navCtrl: NavController) { }
+
+  navigateTo(page: string) {
+    this.navCtrl.navigateForward(`/${page}`);
+  }
 }
