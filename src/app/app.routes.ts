0419dc46d94f7555709ca1d5f838beba5dbb2cca
DarioCostaAlves <dario.c.alves22@gmail.com>
2024-06-09 17:36:56 +0100

    added the rest of the crud for clients. commented for tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,11 @@ export const routes: Routes = [
     loadComponent: () => import('./clients/clients-list/clients-list.component').then((m) => m.ClientsListComponent),
   },
   {
+    path: 'client-detail',
+    loadComponent: () => import('./clients/client-detail/client-detail.component').then((m) => m.ClientDetailComponent),
+  
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -24,3 +24,49 @@ export class ClientService {
   
   
 }
+
+
+
+
+//Remover//
+/*deleteClient_REStAPI(idCliente: number): Observable<Client> {
+  return this.httpCliente
+  .delete<Client>(`${this.baseUrl}/clients/${idClient}/')
+  .pipe(
+    tap((_) => console.log(`removeu`)),
+    catchError(this.handleError<Client>(`Apagar Client ${idClient}`))
+  );
+}*/
+
+
+
+/*onDeleteClient(){
+  this.alertCtrl
+  .create({
+    header: 'Confirmação',
+    message: 'Deseja eliminar este registo de Cliente?',
+    buttons: [
+      {text: 'Cancelar', role: 'cancel'},
+      {text: 'Eliminar',
+        handle: () => {
+          this.ClientService.deleteClient_REStAPI(this.loadedClient.id).
+          subscribe(_ => {
+            this.zone.run(() => this.router.navigate(['/clients']));
+          })
+        },
+      },
+    ],
+  })
+  .then((alert) => alert.present());
+}*/
+
+
+
+
+
+
+
+
+
+
+
